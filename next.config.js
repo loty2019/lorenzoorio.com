ccaed82eae88ccbd22340fbf4e9dd52a8dcf552b
Lorenzo <lorenzo.orio@icloud.com>
2025-06-11 14:04:09 -0400

    Allow more Google thumbnail domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["drive.google.com", "lh3.googleusercontent.com"],
+    domains: [
+      "drive.google.com",
+      "lh3.googleusercontent.com",
+      "lh4.googleusercontent.com",
+      "lh5.googleusercontent.com",
+      "lh6.googleusercontent.com",
+    ],
   },
 }
 
